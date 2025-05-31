69f285c24812e1aaafdde69c1299a9bac7750643
Thiago Mota <euthiagojose2004@gmail.com>
2025-05-31 01:00:01 -0300

    feat: implementation swagger with jwt  authentication v0.4.5

--- src/main/java/br/com/e_comerce/services/CategoryService.java
+++ src/main/java/br/com/e_comerce/services/CategoryService.java
@@ -3,8 +3,8 @@ package br.com.e_comerce.services;
 import br.com.e_comerce.dto.CreateCategoryDto;
 import br.com.e_comerce.dto.ResponseCategoryDto;
 import br.com.e_comerce.entities.Category;
+import br.com.e_comerce.exceptions.CategoryAlreadyExistsException;
 import br.com.e_comerce.repositories.CategoryRepository;
-import java.lang.RuntimeException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,14 +17,15 @@ public class CategoryService {
     private CategoryRepository categoryRepository;
 
     public ResponseCategoryDto create(CreateCategoryDto dto) {
+
         if (categoryRepository.existsByName(dto.name())) {
-            throw new RuntimeException("Categoria já cadastrada.");
+            throw new CategoryAlreadyExistsException("Categoria já cadastrada.");
         }
 
-        Category category = Category.builder().name(dto.name()).build();
-        category = categoryRepository.save(category);
+        Category category = new Category(dto.name());
+        var categotySaved = category = categoryRepository.save(category);
 
-        return new ResponseCategoryDto(category);
+        return new ResponseCategoryDto(categotySaved);
     }
 
     public List<ResponseCategoryDto> findAll() {
