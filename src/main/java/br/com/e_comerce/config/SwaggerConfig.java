cce9626559e0defeb8cccfaa4937687d6b27ee50
Thiago Mota <euthiagojose2004@gmail.com>
2025-06-08 19:02:06 -0300

    fix: correction on the project CORS config

--- src/main/java/br/com/e_comerce/config/SwaggerConfig.java
+++ src/main/java/br/com/e_comerce/config/SwaggerConfig.java
@@ -8,25 +8,23 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
 
-   @Bean
-public OpenAPI customOpenAPI() {
-    final String securitySchemeName = "bearerAuth";
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
 
-    return new OpenAPI()
-            .info(new Info().title("E-Commerce API").version("v1"))
-            .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-            .components(new Components()
-                    .addSecuritySchemes(securitySchemeName,
-                            new SecurityScheme()
-                                    .name(securitySchemeName)
-                                    .type(SecurityScheme.Type.HTTP)
-                                    .scheme("bearer")
-                                    .bearerFormat("JWT")))
-            .addServersItem(new Server().url("https://e-comerce-backend-production.up.railway.app"));
-}
+        return new OpenAPI()
+                .info(new Info().title("E-Commerce API").version("v1"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                .addSecuritySchemes(securitySchemeName,
+                new SecurityScheme()
+                .name(securitySchemeName)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")));
+    }
 }
