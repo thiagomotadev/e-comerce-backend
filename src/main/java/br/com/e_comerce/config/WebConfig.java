2b61c9f1c685b22001bb7fb06e421f52945ca639
Thiago Mota <euthiagojose2004@gmail.com>
2025-06-08 18:21:17 -0300

    fix: correction CORS

---
+++ src/main/java/br/com/e_comerce/config/WebConfig.java
@@ -0,0 +1,17 @@
+package br.com.e_comerce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "https://e-comerce-backend-production.up.railway.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
