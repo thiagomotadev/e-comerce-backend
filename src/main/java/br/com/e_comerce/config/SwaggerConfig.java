b8179f00eae2a1dd79b7ee3163a6dbdaf5632cb0
Thiago Mota <euthiagojose2004@gmail.com>
2025-06-09 02:48:08 -0300

    fix: CORD ERROR Correction

--- src/main/java/br/com/e_comerce/config/SwaggerConfig.java
+++ src/main/java/br/com/e_comerce/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package br.com.e_comerce.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,12 +13,16 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${swagger.server-url:https://e-comerce-backend-production.up.railway.app}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI customOpenAPI() {
         final String securitySchemeName = "bearerAuth";
 
         return new OpenAPI()
                 .info(new Info().title("E-Commerce API").version("v1"))
+                .addServersItem(new Server().url(serverUrl))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(new Components()
                 .addSecuritySchemes(securitySchemeName,
