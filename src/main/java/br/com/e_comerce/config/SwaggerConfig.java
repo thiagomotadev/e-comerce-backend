18121a0e7f2cde72cb1dd72a9669999810f580c6
Thiago Mota <euthiagojose2004@gmail.com>
2025-06-09 02:49:49 -0300

    fix: CORD ERROR Correction

--- src/main/java/br/com/e_comerce/config/SwaggerConfig.java
+++ src/main/java/br/com/e_comerce/config/SwaggerConfig.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
